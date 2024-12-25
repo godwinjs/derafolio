f2ad5e9d85684c172adfcda7faeda55a249963df
Godwin Iyke <godwinikechukwu.dev@gmail.com>
2024-12-25 04:32:16 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,214 @@
+// Copyright Year
+let date = document.querySelector('#date');
+const d = new Date;
+date.innerHTML = d.getFullYear();
+// Copyright Year End
+
+//
+$(window).on("load", function() {
+
+	$(".loader .inner").fadeOut(500, function() {
+		$(".loader").fadeOut(750);
+	});
+
+})
+// 
+
+
+// 
+$(document).ready(function() {
+
+	$('#slides').superslides({
+		animation: 'fade',
+		play: 5000,
+		pagination: false
+	});
+
+	var typed = new Typed(".typed", {
+		strings: ["UI/UX Designer.", "Graphics Designer.", "Student.", "Entrepreneur."],
+		typeSpeed: 70*1.5,
+		loop: true,
+		startDelay: 200,
+		showCursor: true,
+		fadeOut: true
+	});
+
+	$('.typed-cursor').addClass('cursor-typed'); 
+	
+	console.log($('.navbar-toggler > span')[0].classList.value == 'navbar-toggler-icon' );
+
+	$('.navbar-toggler').click( function(){
+
+		if($('.navbar-toggler > span')[0].classList.value == 'navbar-toggler-icon'){
+			$('.navbar-toggler > span').toggleClass();			
+			$('.navbar-toggler > span').addClass('bi-x-lg white');
+		}else{
+			$('.navbar-toggler > span').toggleClass();
+			$('.navbar-toggler > span').addClass('navbar-toggler-icon');
+		}
+
+	});
+
+
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    items: 4,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        480:{
+	            items:2
+	        },
+	        768:{
+	            items:3
+	        },
+	        938:{
+	            items:4
+	        }
+	    }
+	});
+
+
+	
+
+
+	var skillsTopOffset = $(".skillsSection").offset().top;
+	var statsTopOffset = $(".statsSection").offset().top;
+	var countUpFinished = false;
+	$(window).scroll(function() {
+
+		if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+
+			$('.chart').easyPieChart({
+		        easing: 'easeInOut',
+		        barColor: '#fff',
+		        trackColor: false,
+		        scaleColor: false,
+		        lineWidth: 4,
+		        size: 152,
+		        onStep: function(from, to, percent) {
+		        	$(this.el).find('.percent').text(Math.round(percent));
+		        }
+		    });
+
+
+		}
+
+
+		if(!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
+			$(".counter").each(function() {
+				var element = $(this);
+				var endVal = parseInt(element.text());
+
+				element.countup(endVal);
+			})
+
+			countUpFinished = true;
+
+		}
+
+
+	});
+
+
+	$("[data-fancybox]").fancybox();
+
+
+	$(".items").isotope({
+		filter: '*',
+		animationOptions: {
+			duration: 1500,
+			easing: 'linear',
+			queue: false
+		}
+	});
+
+	$("#filters a").click(function() {
+
+		$("#filters .current").removeClass("current");
+		$(this).addClass("current");
+
+		var selector = $(this).attr("data-filter");
+
+		$(".items").isotope({
+			filter: selector,
+			animationOptions: {
+				duration: 1500,
+				easing: 'linear',
+				queue: false
+			}
+		});
+
+		return false;
+	});
+
+
+
+	$("#navigation li a").click(function(e) {
+		e.preventDefault();
+
+		var targetElement = $(this).attr("href");
+		var targetPosition = $(targetElement).offset().top;
+		$("html, body").animate({ scrollTop: targetPosition - 50 }, "slow");
+
+	});
+
+
+
+
+	const nav = $("#navigation");
+	const navTop = nav.offset().top;
+
+	$(window).on("scroll", stickyNavigation);
+
+	function stickyNavigation() {
+
+		var body = $("body");
+
+		if($(window).scrollTop() >= navTop) {
+			body.css("padding-top", nav.outerHeight() + "px");
+			body.addClass("fixedNav");
+		}
+		else {
+			body.css("padding-top", 0);
+			body.removeClass("fixedNav");
+		}
+
+
+
+
+	}
+
+	function updatePopoverTrigger() {
+		const triggerType = window.innerWidth < 768 ? 'click' : 'hover';
+		$('[data-toggle="popover"]').popover('dispose'); // Destroy any existing popovers
+		$('[data-toggle="popover"]').popover({
+		  trigger: triggerType
+		});
+	  }
+  
+	  // Set trigger on page load
+	  updatePopoverTrigger();
+  
+	  // Update trigger on window resize
+	  $(window).on('resize', updatePopoverTrigger);
+
+});
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
